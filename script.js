07102818d6a8478b3238ccfdd8d05635a354ab88
Vitor de Oliveira Araújo <109856517+vitor-oliveira-3km@users.noreply.github.com>
2022-09-11 00:59:11 -0300

    commit init

---
+++ script.js
@@ -0,0 +1,19 @@
+let loginForm = document.querySelector(".header .login-form");
+
+document.querySelector("#login-btn").onclick = () =>{
+    loginForm.classList.toggle("active");
+    navbar.classList.remove("active");
+}
+
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');    
+    loginForm.classList.remove('active');
+    
+}
+
+window.onscroll = () =>{
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
